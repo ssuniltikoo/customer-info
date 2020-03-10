b68e3f801926f84e26e24f8bf2a915dc8cbd72eb
sunil tikoo <ssunil.tikoo@gmail.com>
2020-03-11 01:15:02 +0530

    docker files added

--- src/main/java/com/customer/info/controller/CustomerInfoController.java
+++ src/main/java/com/customer/info/controller/CustomerInfoController.java
@@ -1,17 +1,25 @@
 package com.customer.info.controller;
 
+
+import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import static jdk.nashorn.internal.runtime.regexp.joni.Config.log;
+
+
 @RestController
 @RequestMapping("/customer/")
+
 public class CustomerInfoController {
 
     @GetMapping(value = "info/{num}")
-    public @ResponseBody ResponseEntity<String> getCustomerinfo(@PathVariable String num){
+    public @ResponseBody
+    ResponseEntity<String> getCustomerinfo(@PathVariable String num) {
         //return "customer info for id :"+num;
-      String cust = "hello cust from cust id "+ num;
-        return new ResponseEntity<>(cust,HttpStatus.OK);
+        String cust = "hello cust from cust id " + num;
+        return new ResponseEntity<>(cust, HttpStatus.OK);
     }
 }
