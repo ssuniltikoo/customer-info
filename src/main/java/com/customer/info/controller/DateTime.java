9dc463033e2cb8f5a7c4d5b1721b550963be9f7a
sunil tikoo <ssunil.tikoo@gmail.com>
2020-03-22 02:04:50 +0530

    updaated date time file

--- src/main/java/com/customer/info/controller/DateTime.java
+++ src/main/java/com/customer/info/controller/DateTime.java
@@ -4,13 +4,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+
 @RestController
 @RequestMapping("/datetime/")
 public class DateTime {
 
-    @GetMapping("/date")
-    public String getTime(){
+    @GetMapping("/localdatetime")
+    public String getLocalDateTime(){
+        LocalDateTime localDateTime = LocalDateTime.now();
+        return  localDateTime.toString();
+        //return "date";
+    }
 
-        return "date";
+
+    @GetMapping("/offsetdatetime")
+    public String getOffsetDateTime(){
+        OffsetDateTime offsetDateTime = OffsetDateTime.now();
+        return  offsetDateTime.toString();
+
     }
 }
